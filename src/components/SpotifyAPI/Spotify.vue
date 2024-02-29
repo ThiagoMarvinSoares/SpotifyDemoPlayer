5685e364e6e84b5f09a49d5e111fd81ee019fb62
Thiago Marvin <thiagomarvin.soares@gmail.com>
2024-02-29 14:41:59 -0300

    Spotify fetch credentials done

---
+++ src/components/SpotifyAPI/Spotify.vue
@@ -0,0 +1,32 @@
+<template>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  name: 'SpotifyAPI',
+  async fetchPlaylists() {
+    try {
+      // POST request
+      const tokenResponse = await axios.post(
+        'https://accounts.spotify.com/api/token',
+        'grant_type=client_credentials&client_id=a6796a45306e48fcaa87626717314bfe&client_secret=2a63f4580d1649a98ad57ef7bfd78426',
+        {
+          headers: {
+            'Content-Type': 'application/x-www-form-urlencoded'
+          }
+        }
+      );
+      
+      // Extract the access token from the response
+      const token = tokenResponse.data.access_token;
+
+      console.log(token);
+      
+    } catch (error) {
+      console.error('Error fetching playlists:', error);
+    }
+  }
+};
+</script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,12 +1,14 @@
 <script setup>
 import SongsList from '../components/Songs/SongsList.vue'
 import ThePlayer from '../components/Player/ThePlayer.vue'
+import SpotifyAPI from '../components/SpotifyAPI/Spotify.vue'
 </script>
 
 <template>
   <main class="app-elements">
     <SongsList />
     <ThePlayer />
+    <SpotifyAPI />
   </main>
 </template>
 
