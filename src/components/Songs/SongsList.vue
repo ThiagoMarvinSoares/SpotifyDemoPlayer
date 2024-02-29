086a26acb49a6ad002ae4208221d52fdc61acd7f
Thiago Marvin <thiagomarvin.soares@gmail.com>
2024-02-29 16:38:36 -0300

    Token successfully being generated

--- src/components/Songs/SongsList.vue
+++ src/components/Songs/SongsList.vue
@@ -19,32 +19,39 @@
 </template>
   
 <script>
+
 //Conection with API
-console.log('Marvin');
+
 import axios from 'axios';
 
 export default {
   name: 'SpotifyAPI',
-  async fetchPlaylists() {
-    try {
-      // POST request
-      const tokenResponse = await axios.post(
-        'https://accounts.spotify.com/api/token',
-        'grant_type=client_credentials&client_id=a6796a45306e48fcaa87626717314bfe&client_secret=2a63f4580d1649a98ad57ef7bfd78426',
-        {
-          headers: {
-            'Content-Type': 'application/x-www-form-urlencoded'
-          }
-        }
-      );
-      
-      // Extract the access token from the response
-      const token = tokenResponse.data.access_token;
+  mounted() {
+    this.fetchPlaylists(); // Call the function here
+  },
+  methods: {
+    async fetchPlaylists() {
 
-      console.log(token);
-      
-    } catch (error) {
-      console.error('Error fetching playlists:', error);
+      try {
+        // POST request
+        const tokenResponse = await axios.post(
+          'https://accounts.spotify.com/api/token',
+          'grant_type=client_credentials&client_id=a6796a45306e48fcaa87626717314bfe&client_secret=2a63f4580d1649a98ad57ef7bfd78426',
+          {
+            headers: {
+              'Content-Type': 'application/x-www-form-urlencoded'
+            }
+          }
+        );
+        
+        // Extract the access token from the response
+        const token = tokenResponse.data.access_token;
+
+        console.log(token);
+        
+      } catch (error) {
+        console.error('Error fetching playlists:', error);
+      }
     }
   }
 };
