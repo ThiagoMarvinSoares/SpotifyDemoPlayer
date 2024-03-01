04752925c9ab98108eb91beabb218f418d6b1fc1
Thiago Marvin <thiagomarvin.soares@gmail.com>
2024-03-01 09:44:30 -0300

    POST request working tree clean + identation done

--- src/components/Player/ThePlayer.vue
+++ src/components/Player/ThePlayer.vue
@@ -5,7 +5,7 @@
         <div>
           <p>image</p>
         </div>
-        <div class="player-card_info">  
+        <div class="player-card_info">
           <p><!--{{ song.title }}-->song</p>
           <p>artist</p>
         </div>
@@ -16,14 +16,12 @@
       <div class="player-volume">
         <p>volume</p>
       </div>
-    </div>      
+    </div>
   </div>
-  
-  </template>
-  
-  <script>
-  </script>
-  
+</template>
+
+<script></script>
+
 <style scoped>
 .wrap {
   width: 100%;
@@ -33,25 +31,23 @@
   align-items: center;
 }
 .player-card {
-    display: flex;
-    flex-direction: row;
-    justify-content: space-around;
-    align-items: center;
-    bottom: 0;
-    position: fixed;
-     /* colors and styles */
-    color: white;
-    background-color: black;
-    width: 100%;
-  }
-  .player-card_left {
-      display: flex;  
-      flex-direction: row;
-    }
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+  align-items: center;
+  bottom: 0;
+  position: fixed;
+  /* colors and styles */
+  color: white;
+  background-color: black;
+  width: 100%;
+}
+.player-card_left {
+  display: flex;
+  flex-direction: row;
+}
 
-    .player-element {
-      margin-right: 40px;
-    }  
-
+.player-element {
+  margin-right: 40px;
+}
 </style>
-  
--- src/components/Songs/SongsList.vue
+++ src/components/Songs/SongsList.vue
@@ -3,10 +3,10 @@
     <div class="song-card">
       <div class="song-card_element">
         <div class="song-card_left">
-        <button>Play</button>
-        <p>image</p>
-      </div>
-        <div class="song-card_info">  
+          <button>Play</button>
+          <p>image</p>
+        </div>
+        <div class="song-card_info">
           <p><!--{{ song.title }}-->song</p>
           <p>artist</p>
         </div>
@@ -17,94 +17,108 @@
     </div>
   </div>
 </template>
-  
-<script>
 
+<script>
 //Conection with API
 
-import axios from 'axios';
+import axios from "axios";
 
 export default {
-  name: 'SpotifyAPI',
+  name: "SpotifyAPI",
   mounted() {
     this.fetchPlaylists(); // Call the function here
   },
   methods: {
     async fetchPlaylists() {
-
       try {
         // POST request
         const tokenResponse = await axios.post(
-          'https://accounts.spotify.com/api/token',
-          'grant_type=client_credentials&client_id=a6796a45306e48fcaa87626717314bfe&client_secret=2a63f4580d1649a98ad57ef7bfd78426',
+          "https://accounts.spotify.com/api/token",
+          "grant_type=client_credentials&client_id=a6796a45306e48fcaa87626717314bfe&client_secret=2a63f4580d1649a98ad57ef7bfd78426",
           {
             headers: {
-              'Content-Type': 'application/x-www-form-urlencoded'
-            }
+              "Content-Type": "application/x-www-form-urlencoded",
+            },
           }
         );
-        
+
         // Extract the access token from the response
         const token = tokenResponse.data.access_token;
-
         console.log(token);
-        
+
+        const headers = {
+          Authorization: "Bearer " + { token },
+        };
+
+        const artistsUrl =
+          "https://api.spotify.com/v1/artists/4Z8W4fKeB5YxbusRsdQVPb";
+
+        const response = await axios.get(artistsUrl, { headers });
+
+        if (response.status === 200) {
+          const artistData = response.data;
+          console.log("Artist information:", artistData);
+        } else {
+          console.error(
+            "Error fetching artist information:",
+            response.statusText
+          );
+        }
       } catch (error) {
-        console.error('Error fetching playlists:', error);
+        console.error("Error fetching playlists:", error);
       }
-    }
-  }
+    },
+  },
 };
 </script>
-  
-  <style lang="scss" scoped>
-  .wrap {
-    background-color: #302B27;
-    width: 30%;
-    height: 100%;
+
+<style lang="scss" scoped>
+.wrap {
+  background-color: #302b27;
+  width: 30%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  padding: 20px;
+  margin: 30px 0;
+  border-radius: 15px;
+}
+.song-card {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  color: black;
+  width: 350px;
+  border-radius: 5px;
+  background-color: white;
+  padding: 15px;
+  margin: 15px;
+
+  &_element {
     display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    padding: 20px;
-    margin: 30px 0;
-    border-radius: 15px;
+    flex-direction: row;
   }
-  .song-card {
+
+  &_left {
     display: flex;
     flex-direction: row;
     align-items: center;
-    justify-content: space-between;
-    color: black;
-    width: 350px;
-    border-radius: 5px;
-    background-color: white;
-    padding: 15px;
-    margin: 15px;
-
-    &_element {
-      display: flex;
-      flex-direction: row;
-    }
-
-    &_left {
-      display: flex;
-      flex-direction: row;
-      align-items: center;
-    }
-
-    &_info {
-      display: flex;
-      flex-direction: column;
-      align-items: center;
-      padding-left: 30px;
-    }
-
-    &_right {
-      display: flex;
-      flex-direction: row;
-      align-items: center;
-    }
   }
-  </style>
-  
+
+  &_info {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding-left: 30px;
+  }
+
+  &_right {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+  }
+}
+</style>
